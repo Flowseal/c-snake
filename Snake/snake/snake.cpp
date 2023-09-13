cd6a4338c022dde2aff869721be299236eaa1d5d
Flowseal <trololo424@yandex.ru>
2023-09-13 17:47:40 +0300

    size_t to int cast fix

--- Snake/snake/snake.cpp
+++ Snake/snake/snake.cpp
@@ -3,7 +3,7 @@
 void Snake::moveSnake( )
 {
 	// Move tail for snake's head
-	for ( int i = mSnakeTail.size( ) - 1; i >= 0; i-- )
+	for ( int i = static_cast<int>(mSnakeTail.size( )) - 1; i >= 0; i-- )
 	{
 		if ( i == 0 )
 			mSnakeTail.at( i ) = mSnakeHead;
@@ -30,30 +30,30 @@ void Snake::moveSnake( )
 
 MoveDirection Snake::getMoveDirection( )
 {
-	return mMoveDirection; 
+	return mMoveDirection;
 }
 
-void Snake::setMoveDirection( MoveDirection moveDirection ) 
-{ 
+void Snake::setMoveDirection( MoveDirection moveDirection )
+{
 	mMoveDirection = moveDirection;
 }
 
-Coord Snake::getSnakeHead( ) 
-{ 
-	return mSnakeHead; 
-}
-
-void Snake::setSnakeHead( Coord snakeHead ) 
-{ 
-	mSnakeHead = snakeHead; 
-}
-
-std::vector<Coord>& Snake::getSnakeTail( ) 
-{ 
-	return mSnakeTail; 
-}
-
-void Snake::setSnakeTail( std::vector<Coord> snakeTail ) 
+Coord Snake::getSnakeHead( )
 {
-	mSnakeTail = snakeTail; 
+	return mSnakeHead;
+}
+
+void Snake::setSnakeHead( Coord snakeHead )
+{
+	mSnakeHead = snakeHead;
+}
+
+std::vector<Coord>& Snake::getSnakeTail( )
+{
+	return mSnakeTail;
+}
+
+void Snake::setSnakeTail( std::vector<Coord> snakeTail )
+{
+	mSnakeTail = snakeTail;
 }
